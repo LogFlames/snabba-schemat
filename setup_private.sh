2bc0e1e49432cbd18bb7600ba7977f5d89e69e05
BuildTools <unconfigured@null.spigotmc.org>
2021-04-30 01:59:37 +0200

    Inclease key size to 2048

--- setup_private.sh
+++ setup_private.sh
@@ -33,9 +33,9 @@ cat > secret.json << EOF
 EOF
 fi
 
-if [ ! -e rsa_1024_priv.pem ]; then
-openssl genrsa -out rsa_1024_priv.pem 1024;
-openssl rsa -pubout -in rsa_1024_priv.pem -out rsa_1024_pub.pem;
+if [ ! -e rsa_2048_priv.pem ]; then
+openssl genrsa -out rsa_2048_priv.pem 2048;
+openssl rsa -pubout -in rsa_2048_priv.pem -out rsa_2048_pub.pem;
 fi
 
 cd ..
