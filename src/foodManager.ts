5752671290637db70af9ffdfdeca4aa67296a5d7
LogFlames <elilun03@gmail.com>
2021-08-18 01:45:33 +0200

    Organize into table, INSTEAD of everything being one row, with one column per day

--- src/foodManager.ts
+++ src/foodManager.ts
@@ -43,70 +43,74 @@ export async function getFood(weeks: string[]): Promise<{ [week: string]: FoodWe
 }
 
 async function scrapeFood(weeks: string[]) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
+
+    await page.setRequestInterception(true);
+    page.on("request", (req) => {
+        if (req.resourceType() == "image" || req.resourceType() == "stylesheet" || req.resourceType() == "font" || req.resourceType() == "script") {
+            req.abort();
+        } else {
+            req.continue();
+        }
+    });
+
     await page.goto('https://sodexo.mashie.com/public/app/St%20Eriks%20gymnasium/6639b607?country=se');
 
     await page.waitForSelector('#app-page > div.panel-group');
 
-    let foodWeekHTMLs: { [week: string]: string } = await page.evaluate((weeks: string[]): { [week: string]: string } => {
+    let foodWeekTexts: { [week: string]: string[][] } = await page.evaluate((weeks: string[]): { [week: string]: string[][] } => {
         let weekHolder = document.querySelector("#app-page > div.panel-group");
 
         if (!weekHolder) {
             return {};
         }
 
-        let weeksHTML: { [week: string]: string } = {};
+        let weeksText: { [week: string]: string[][] } = {};
         let thisWeek: string = "";
 
         let i = 0;
-        let extracted = document.createElement("tr");
         while (weekHolder.childElementCount > i) {
-            if (weekHolder.children[i].nodeName === "H4" && Object.keys(weeksHTML).length === weeks.length) {
+            if (weekHolder.children[i].nodeName === "H4" && Object.keys(weeksText).length >= weeks.length) {
                 break;
             }
 
             if (weekHolder.children[i].nodeName === "H4") {
-                if (thisWeek) {
-                    weeksHTML[thisWeek] = "<table>" + extracted.outerHTML + "</table>";
-                    extracted = document.createElement("tr");
-                    thisWeek = "";
-                }
-
                 let mat = (weekHolder.children[i] as HTMLElement).innerText.match(/[\d]+/g);
                 if (mat) {
                     let week = mat[0];
                     if (weeks.includes(week)) {
                         thisWeek = week;
+                        weeksText[thisWeek] = [];
+                    } else {
+                        thisWeek = "";
                     }
                 }
             } else if (weekHolder.children[i].nodeName === "DIV") {
                 if (thisWeek) {
-                    let holder = document.createElement("td");
+                    let holderList = [];
 
                     let dateDay = document.createElement("h3");
                     dateDay.innerText = (weekHolder.children[i].children[0] as HTMLElement).innerText;
-                    holder.appendChild(dateDay);
+                    holderList.push(dateDay.outerHTML);
 
                     let numberMatch = dateDay.innerText.match(/[\d]+/g);
                     if (!numberMatch) {
                         thisWeek = "";
                     } else {
-                        holder.id = `day_${('0' + numberMatch[0]).slice(-2)}`;
-
                         for (let j = 0; j < weekHolder.children[i].children[1].childElementCount; j++) {
                             let category = document.createElement("strong");
                             category.innerText = (weekHolder.children[i].children[1].children[j].children[1] as HTMLElement).innerText;
-                            holder.appendChild(category);
+                            holderList.push(category.outerHTML);
 
                             let food = document.createElement("p");
                             food.innerText = (weekHolder.children[i].children[1].children[j].children[2] as HTMLElement).innerText;
                             food.style.marginTop = "4px";
                             food.style.marginBottom = "12px";
-                            holder.appendChild(food);
+                            holderList.push(food.outerHTML);
                         }
 
-                        extracted.appendChild(holder);
+                        weeksText[thisWeek].push(holderList);
                     }
                 }
             }
@@ -114,14 +118,30 @@ async function scrapeFood(weeks: string[]) {
             i++;
         }
 
-        if (thisWeek) {
-            weeksHTML[thisWeek] = "<table>" + extracted.outerHTML + "</table>";
-        }
-
-        return weeksHTML;
+        return weeksText;
     }, weeks);
 
     await browser.close();
+
+    let foodWeekHTMLs: {[week: string]: string} = {};
+
+    for (let week in foodWeekTexts) {
+        foodWeekHTMLs[week] = "<table>";
+        let maxLength = Math.max(...(foodWeekTexts[week].map(el => el.length)));
+        for (let j = 0; j < maxLength; j++) {
+            let row = "<tr>";
+            for (let i = 0; i < foodWeekTexts[week].length; i++) {
+                if (j >= foodWeekTexts[week][i].length) {
+                    row += "<td></td>";
+                } else {
+                    row += "<td>" + foodWeekTexts[week][i][j] + "</td>";
+                }
+            }
+            row += "</tr>";
+            foodWeekHTMLs[week] += row;
+        }
+        foodWeekHTMLs[week] += "</table>";
+    }
 
     let now = new Date().toUTCString();
     for (let week of weeks) {
