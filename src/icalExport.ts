45afd0c8c0662343eb036cf2a9ba13a4dab84dda
LogFlames <elilun03@gmail.com>
2022-09-12 01:32:21 +0200

    Use windows line endings and escape commas

--- src/icalExport.ts
+++ src/icalExport.ts
@@ -69,6 +69,9 @@ END:VEVENT
 
     ical += "END:VCALENDAR\n";
 
+    ical = ical.replace(/,/g, "\\,");
+    ical = ical.replace(/\n/g, "\r\n");
+
     if (!fs.existsSync(path.join(__dirname, "..", "temp", "export-ics"))) {
         fs.mkdirSync(path.join(__dirname, "..", "temp", "export-ics"), { recursive: true });
     }
